c2c664c9ef149d293d09c5983fb565d4e84efa47
Omar Gamal <omargamal.2tecno@gmail.com>
2023-09-28 12:44:52 +0200

    push piape and details

--- src/app/movie-details/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details/movie-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { timeInterval } from 'rxjs';
 import { MovieDetails } from 'src/app/interfaces/movie';
 import { ApiService } from 'src/app/shared/services/api.service';
@@ -12,13 +13,17 @@ export class MovieDetailsComponent implements OnInit {
   data!: MovieDetails;
   imagePoster!: string;
   id!: number;
-  constructor(private apiServ: ApiService) {}
+  dropImage: string | undefined;
+  constructor(private apiServ: ApiService, private activateRoute: ActivatedRoute) {
+
+  }
   ngOnInit() {
-    this.id = this.apiServ.snapshot.params["id"]
+    this.id = this.activateRoute.snapshot.params["id"]
     this.apiServ.getMoviesById(this.id).subscribe((val) => {
       this.data = val;
       console.log(this.data);
       this.imagePoster = this.apiServ.getImgSrcByPosterPath(val.poster_path);
+      this.dropImage = this.apiServ.getImgSrcByPosterPath(val.backdrop_path);
     });
   }
 }
