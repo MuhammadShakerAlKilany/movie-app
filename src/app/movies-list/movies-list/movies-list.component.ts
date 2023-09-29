13ea20cac0c2940509ba369496bb66d8e21f922b
Muhammad Shaker Kasem AlKilany <m.sh64944@gmail.com>
2023-09-29 16:29:40 +0300

    pagination home page

--- src/app/movies-list/movies-list/movies-list.component.ts
+++ src/app/movies-list/movies-list/movies-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/movie';
 import { ApiService } from 'src/app/shared/services/api.service';
+import { PaginationHomePageService } from 'src/app/shared/services/pagination-home-page.service';
 
 
 @Component({
@@ -11,13 +12,36 @@ import { ApiService } from 'src/app/shared/services/api.service';
 export class MoviesListComponent implements OnInit{
   movies!: Movie[];
   imgSrc!: string;
+  pageNumper!:number
+  pageNumperToShow!:number
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService , private paginationHomePageService:PaginationHomePageService) { }
   ngOnInit() {
-    this.api.getAllMoviesPopularByPageNumber().subscribe((val) => {this.movies = val.results
+    // this.api.getAllMoviesPopularByPageNumber().subscribe((val) => {this.movies = val.results})
+    if(!this.movies){
+      this.paginationHomePageService.getPage()
+    }
+    this.paginationHomePageService.homePage.subscribe((data)=>{
+      console.log("data")
+      this.movies = data.results
+      this.pageNumper = data.page
+      this.pageNumperToShow = this.pageNumper
     })
-
+    
   }
-
+  createRange(number:number){
+    return new Array(number).fill(0)
+      .map((n, index) => index );
+  }
+  getPage(numPage:any,element:HTMLElement){
+    this.paginationHomePageService.getPage(numPage)
+    element.blur() 
+  }
+  nextPage(){
+    this.pageNumperToShow +=1
+  }
+  previousPage(){
+    this.pageNumperToShow -=1
+  }
 
 }
